86ca466ab82ffbc960f832b78b9519e1f0d8d178
Tommy Bishop <bishop.thomas@gmail.com>
2014-08-11 16:50:00 -0700

    bump to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## head
 
+## 0.1.1 (08/11/2014)
+
 * remove unused reference to `intu_metadata` namespace
 
 ## 0.1.0 (02/15/2013)
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "richard_mendes@intuit.com"
 license          "Apache 2.0"
 description      "Installs/Configures the yum-s3-iam plugin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w{ centos rhel }.each do |os|
   supports os
