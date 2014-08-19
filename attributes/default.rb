4d20303a192c50345a2aec5f6767fffe9644ff57
Etienne Grignon <etienne_grignon@intuit.com>
2014-08-19 16:29:23 -0700

    adding multiple repository setup via attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default['yum_s3_iam']['version'] = '1.0.1-1'
 default['yum_s3_iam']['repo_name'] = 'our repo'
 default['yum_s3_iam']['repo_description'] = 'our package repo'
 default['yum_s3_iam']['repo_bucket'] = 'http://our-bucket-name.s3.amazonaws.com/'
+default['yum_s3_iam']['repo_default'] = true
+default['yum_s3_iam']['repositories'] = {}
